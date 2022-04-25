0c4eb33839154e178e47fbcb6f8687df89ce051b
Sachin <sachin97.bs@gmail.com>
2022-04-25 16:19:18 -0500

    K-means computed

--- ClusterModel.R
+++ ClusterModel.R
@@ -16,15 +16,19 @@ library(hrbrthemes)
 library(viridis)
 library(tseries)
 
-df <- fread('./pollutant dataframes/Carbon_monoxide.csv')
+df <- fread('./pollutant dataframes/Sulfur_dioxide.csv')
 
-consider_cols <- c('Latitude','Longitude','AQI', 'month','day')
+#consider_cols <- c('Latitude','Longitude','AQI', 'month','day')
+consider_cols <- c('AQI')
 data <- subset(df, select = consider_cols)
-data$Latitude <- scale(data$Latitude, center = TRUE, scale = TRUE)
-data$Longitude <- scale(data$Longitude, center = TRUE, scale = TRUE)
-data$AQI <- scale(data$AQI, center = TRUE, scale = TRUE)
 
-# Elbow method to find best number of clusters.
+#data$Latitude <- scale(data$Latitude, center = TRUE, scale = TRUE)
+#data$Longitude <- scale(data$Longitude, center = TRUE, scale = TRUE)
+#data$AQI <- scale(data$AQI, center = TRUE, scale = TRUE)
+#data$month <- scale(data$month, center = TRUE, scale = TRUE)
+#data$day <- scale(data$day, center = TRUE, scale = TRUE)
+
+################## Elbow method to find best number of clusters.################
 set.seed(100)
 wcss <- vector()
 for (i in 1:10) {wcss[i] <- sum(kmeans(x = data, centers = i)$withinss)}
@@ -36,4 +40,20 @@ plot(x = 1:10,
      xlab = "Number of clusters",
      ylab = "WCSS",
      color = 'blue')
+
+####################### K-means clustering ###########################
+set.seed(100)
+clusters <- kmeans(x = data,
+                      centers = 3,
+                      iter.max = 500,
+                      nstart = 20)
+
+########################## PLotly 3d plot ###########################
+fig <- plot_ly(data = df,
+               x = ~Latitude,
+               y = ~Longitude,
+               z = ~AQI,
+               color = clusters$cluster)
+               #colors = c('#BF382A', '#0C4B8E'))
+fig
 
