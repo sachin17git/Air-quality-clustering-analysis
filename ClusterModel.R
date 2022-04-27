41ee1e3770dceef82c3b7d89887b5021185890d1
Sachin <sachin97.bs@gmail.com>
2022-04-27 14:45:30 -0500

    Maps statewise aqi

--- ClusterModel.R
+++ ClusterModel.R
@@ -74,6 +74,37 @@ get_cat_aqi <- function (clust, aqi, nclust, pol) {
   return (aqi)
 }
 
+######################### Filter state/county for dataframes ###########################
+
+filterPlot_maps <- function (state, df, category_aqi) {
+
+  states_indexes <- which(df$'State Name'==state)
+  data <- df[states_indexes, ]
+  new_cat_aqi <- category_aqi[states_indexes]
+
+  fig <- data %>% plot_mapbox(lat = ~Latitude, lon = ~Longitude,
+                            split = as.factor(new_cat_aqi), size=2,
+                            mode = 'scattermapbox', hoverinfo='name')
+
+  fig <- fig %>% layout(title = sprintf("USA, 2021 %s - %s", state, unique(data$'Parameter Name')),
+                       font = list(color='white'),
+                       plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
+                       mapbox = list(style = 'dark'),
+                       legend = list(orientation = 'h',
+                                     font = list(size = 8)),
+                       margin = list(l = 25, r = 25,
+                                     b = 25, t = 25,
+                                     pad = 2, zoom = 20))
+
+  fig <- fig %>% add_trace(marker = list(
+                           size = 10))
+
+  fig <- fig %>% config(mapboxAccessToken = Sys.getenv("MAPBOX_TOKEN"))
+
+  return (fig)
+
+}
+
 ####################### K-means clustering ###########################
 
 kmeans_compute <- function (df, cols, nclusters, pollutant) {
@@ -112,24 +143,32 @@ kmeans_compute <- function (df, cols, nclusters, pollutant) {
 
 }
 ########################## PLotly 3d plot for k-means ###########################
-df <- fread('./pollutant dataframes/Carbon_monoxide.csv')
-fig_aqi <- kmeans_compute(df, consider_cols, 2, "CO")
-fig_aqi[1] #This is the figure object. Unpacking two variables present in the list. fig_aqi[2] is another variable.
+df_co <- fread('./pollutant dataframes/Carbon_monoxide.csv')
+fig_aqi_co <- kmeans_compute(df_co, consider_cols, 2, "CO")
+fig_aqi_co[[1]] #This is the figure object. Unpacking two variables present in the list. fig_aqi[2] is another variable.
 
-df <- fread('./pollutant dataframes/Nitrogen_dioxide_(NO2).csv')
-fig_aqi <- kmeans_compute(df, consider_cols, 2, "NO2")
-fig_aqi[1]
+df_no <- fread('./pollutant dataframes/Nitrogen_dioxide_(NO2).csv')
+fig_aqi_no <- kmeans_compute(df_no, consider_cols, 2, "NO2")
+fig_aqi_no[[1]]
 
-df <- fread('./pollutant dataframes/Ozone.csv')
-fig_aqi <- kmeans_compute(df, consider_cols, 4, "Ozone")
-fig_aqi[1]
+df_ozone <- fread('./pollutant dataframes/Ozone.csv')
+fig_aqi_ozone <- kmeans_compute(df_ozone, consider_cols, 4, "Ozone")
+fig_aqi_ozone[[1]]
 
-df <- fread('./pollutant dataframes/Sulfur_dioxide.csv')
-fig_aqi <- kmeans_compute(df, consider_cols, 3, "SO2")
-fig_aqi[1]
+df_so <- fread('./pollutant dataframes/Sulfur_dioxide.csv')
+fig_aqi_so <- kmeans_compute(df_so, consider_cols, 3, "SO2")
+fig_aqi_so[[1]]
 
 ########################## Visualization Maps ################################
 
 mapbox_token <- "pk.eyJ1IjoiYmFsYXN1YjQiLCJhIjoiY2wyZ3EwbGdrMDNwdDNjbnFmbGtjaHozOCJ9.v3Hi5eF-UYkbpzmqz6_Y6g"
 Sys.setenv("MAPBOX_TOKEN" = mapbox_token) # for Orca
+
+filterPlot_maps(state = "California", df_co, fig_aqi_co[[2]]) # Maps for CO
+
+filterPlot_maps(state = "California", df_no, fig_aqi_no[[2]]) # Maps for No2
+
+filterPlot_maps(state = "Texas", df_ozone, fig_aqi_ozone[[2]]) # Maps for Ozone
+
+filterPlot_maps(state = "Texas", df_so, fig_aqi_so[[2]]) # Maps for So2
 
